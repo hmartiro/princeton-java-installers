a236059ea2b52666d9d32bec737be9623aa3f5f7
Hayk <hayk@ubuntu.(none)>
2012-06-27 23:37:50 -0400

    Adding all initial files.

---
+++ algs4_mac_2.0/algs4.app/Contents/Resources/launcher.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# The installation directory
+name=algs4
+
+# Executing directory of the script
+myDir="$( cd "$( dirname "$0" )" && pwd )"
+
+# Runs the installation script, teeing to a log file
+bash ${myDir}/${name}.sh 2>&1 | tee ~/log.txt
+
+# Takes out the color coding from the log file
+ESC=`echo -e "\033"`
+sed -iE 's,'${ESC}[\[][0-9]*[a-zA-Z]',,g' ~/log.txt
+rm ~/log.txtE
+
+# Moves the log to its final location
+mv ~/log.txt ~/${name}/log.txt
+
+# Sets the owner of everything back to the original,
+# since this script runs under root.
+chown -R ${1} ~/${name}
+chown -R ${1} /Applications/DrJava.app
+chown -R ${1} ~/.drjava
+chown -R ${1} ~/Library/Java/Extensions
---
+++ introcs_mac_2.0/introcs.app/Contents/Resources/introcs.sh
@@ -0,0 +1,346 @@
+#!/bin/bash
+
+# ************************************************
+# introcs.sh                                     
+# Hayk Martirosyan  
+# -------------
+# Mac OS X installation script for introcs.app     
+# Last edited: December 4, 2011
+# ************************************************
+
+# The only difference between algs4 and introcs installations are these
+# three variables, the commenting out of Java3D, and renumbering steps
+name=introcs
+url_base=http://introcs.cs.princeton.edu/java/mac
+testFile=TestIntroCS
+
+### COLOR PRINTING FUNCTIONS ###############################################
+
+function normal {
+	tput sgr0
+}
+function red { 
+	tput setaf 1
+	print "${1}"
+	normal
+}
+function green { 
+	tput setaf 2 
+	print "${1}"
+	normal
+}
+function blue { 
+	tput setaf 4
+	print "${1}"
+	normal
+}
+function print {
+	echo "${1}"
+}
+
+### INTRODUCTION SCREEN ####################################################
+
+# Ensures terminal emulator can handle color
+export TERM=xterm-color
+
+# Creating the install directory
+install=~/${name}
+
+clear
+
+green '####################################################################'
+green '#                                                                  #'
+green '#  8888888          888                     .d8888b.   .d8888b.    #'
+green '#    888            888                    d88P  Y88b d88P  Y88b   #'
+green '#    888            888                    888    888 Y88b.        #'
+green '#    888   88888b.  888888 888d888 .d88b.  888         "Y888b.     #'
+green '#    888   888 "88b 888    888P"  d88""88b 888            "Y88b.   #'
+green '#    888   888  888 888    888    888  888 888    888       "888   #'
+green '#    888   888  888 Y88b.  888    Y88..88P Y88b  d88P Y88b  d88P   #'
+green '#  8888888 888  888  "Y888 888     "Y88P"   "Y8888P"   "Y8888P"    #'
+green '#                                                                  #'
+green '####################################################################'
+green '#                                                                  #'
+green '# Java Programming Environment Setup                               #'
+green '# for Apple OS X - v2.0                                            #'
+green '# Princeton University - Hayk Martirosyan                          #'
+green '#                                                                  #'
+green '####################################################################'
+
+print
+print 'Initializing functions and beginning installation...'
+
+### UTILITY FUNCTIONS  #####################################################
+
+# Executing directory of the script
+myDir="$( cd "$( dirname "$0" )" && pwd )"
+
+# Handy to supress output
+null="/dev/null"
+
+function download {
+	print
+	print "Downloading ${3} from"
+	blue "${1}"
+	print "to"
+	blue "${2}"
+	curl -sL "${1}" > "${2}"
+}
+
+function downloadQuiet {
+	curl -sL "${1}" > "${2}"
+}
+
+function extractAndDelete {
+	print
+	print "Extracting zip archive in place at"
+	blue "${1}"
+	dest=`dirname ${1}`
+	unzip -qqo "${1}" -d "${dest}"
+	print "and deleting .zip file."
+	rm -f "${1}"
+}
+
+function deleteOldVersion {
+	if [ -d "${1}" ]; then
+		print
+		print "Deleting old version of ${2} at"
+		blue "${1}"
+		rm -rf "${1}"
+	fi
+}
+
+function replaceInFile {
+	print
+	print "Replacing text in file"
+	blue "${1}"
+	print "from"
+	blue "${2}"
+	print "to"
+	blue "${3}"
+	sed -i '' 's|'${2}'|'${3}'|g' "${1}"
+}
+
+function createFolderIfNeeded {
+	if [ ! -d "${1}" ]; then
+		print
+		print "Creating ${2} directory at"
+		blue "${1}"
+		mkdir "${1}"
+	fi
+}
+
+### INITIALIZATION AND PRE-INSTALLATION SETUP ############################
+
+createFolderIfNeeded "$install" "installation"
+
+local="/usr/local"
+createFolderIfNeeded "${local}" "user local"
+bin="${local}/bin"
+createFolderIfNeeded "$bin" "bin"
+
+sleep 4
+
+### BEGINNING OF ACTUAL INSTALLATION #####################################
+
+print
+red '#### Step 1 - Java #################################################'
+
+if [ `which -a java` ]; then
+	print
+	print 'Java is already installed.'
+else
+	print
+	red 'No Java runtime detected.'
+	red 'Install Java 6 Runtime Environment, then retry this installer.'
+	print
+	java -version
+	print
+	red 'You can close this window'
+	exit
+fi
+
+print
+red '#### Step 2 - Java3D and Java OpenGL ###############################'
+
+javaLib=~/Library/Java
+createFolderIfNeeded "$javaLib" "Java library"
+
+extensions=~/Library/Java/Extensions
+createFolderIfNeeded "$extensions" "Java extensions"
+
+java3d="${install}/java3d"
+deleteOldVersion "$java3d" "Java3D"
+
+java3dURL="${url_base}/java3d.zip"
+java3dZip="${install}/java3d.zip"
+download "$java3dURL" "$java3dZip" "Java3D"
+
+extractAndDelete "$java3dZip"
+
+print
+print "Copying all libraries in"
+blue "${java3d}"
+print "to"
+blue "${extensions}"
+cp -R ${java3d}/* "${extensions}"
+
+print
+red '#### Step 3 - Textbook Libraries ##################################'
+
+stdlib="${install}/stdlib.jar"
+algs4="${install}/algs4.jar"
+
+stdlibURL="http://introcs.cs.princeton.edu/java/stdlib/stdlib.jar"
+download "$stdlibURL" "$stdlib" "stdlib.jar"
+
+print
+print "Copying library from"
+blue "${stdlib}"
+print "to"
+blue "${extensions}"
+cp ${stdlib} ${extensions}
+
+algs4URL="http://algs4.cs.princeton.edu/code/algs4.jar"
+download "$algs4URL" "$algs4" "algs4.jar"
+
+print
+print "Copying library from"
+blue "${algs4}"
+print "to"
+blue "${extensions}"
+cp ${algs4} ${extensions}
+
+print
+red '#### Step 4 - Checkstyle ##########################################'
+
+checkstyleWild="${install}/checkstyle-?.?"
+deleteOldVersion "$checkstyle" "checkstyle"
+
+checkstyleZip="${install}/checkstyle.zip"
+checkstyleURL="${url_base}/checkstyle.zip"
+download "$checkstyleURL" "$checkstyleZip" "checkstyle"
+
+extractAndDelete "$checkstyleZip"
+
+checkstyle="`print ${checkstyleWild}`"
+checkstyleXML="${checkstyle}/checkstyle.xml"
+checkstyleXMLURL="${url_base}/checkstyle.xml"
+download "$checkstyleXMLURL" "$checkstyleXML" "checkstyle configuration file"
+
+checkstyleCMD="${bin}/checkstyle"
+checkstyleCMDURL="${url_base}/checkstyle"
+download "$checkstyleCMDURL" "$checkstyleCMD" "checkstyle execution script"
+
+print
+print "Granting executable permission to"
+blue "${checkstyleCMD}"
+chmod +x "$checkstyleCMD"
+
+#replaceInFile "${checkstyleCMD}" "INSTALL_DIR" "$install"
+
+print
+red '#### Step 5 - Findbugs ############################################'
+
+findbugsWild="${install}/findbugs-?.?.?"
+deleteOldVersion "$findbugsWild" "findbugs"
+
+findbugsZip="${install}/findbugs.zip"
+findbugsURL="${url_base}/findbugs.zip"
+download "$findbugsURL" "$findbugsZip" "findbugs"
+
+extractAndDelete "$findbugsZip"
+
+findbugs="`print ${findbugsWild}`"
+findbugsXML="${findbugs}/findbugs.xml"
+findbugsXMLURL="${url_base}/findbugs.xml"
+download "$findbugsXMLURL" "$findbugsXML" "findbugs configuration file"
+
+findbugsCMD="${bin}/findbugs"
+findbugsCMDURL="${url_base}/findbugs"
+download "$findbugsCMDURL" "$findbugsCMD" "findbugs execution script"
+
+print
+print "Granting executable permission to"
+blue "${findbugsCMD}"
+chmod +x "$findbugsCMD"
+
+#replaceInFile "${findbugsCMD}" "INSTALL_DIR" "$install"
+
+print
+red '#### Step 6 - DrJava ##############################################'
+
+drjavaApp="/Applications/DrJava.app"
+deleteOldVersion "$drjavaApp" "DrJava"
+
+drjava="/Applications/drjava.tar.gz"
+drjavaURL="${url_base}/drjava-osx.tar.gz"
+download "$drjavaURL" "$drjava" "DrJava"
+
+print
+print 'Extracting DrJava into the Applications directory'
+blue "${drjavaApp}"
+cd "/Applications"
+tar xzf "$drjava"
+rm "$drjava"
+
+drjavaConfig=~/.drjava
+drjavaConfigURL="${url_base}/drjava-config.txt"
+download "$drjavaConfigURL" "$drjavaConfig" "DrJava configuration file"
+
+replaceInFile "${drjavaConfig}" "INSTALL_DIR" "${install}"
+
+print
+print 'Creating a shortcut to DrJava on the desktop...'
+ln -sf "$drjavaApp" ~/Desktop
+
+print
+red '#### Step 7 - Terminal #############################################'
+
+terminalApp="/Applications/Utilities/Terminal.app"
+
+print
+print 'Creating a shortcut to Terminal on the desktop...'
+ln -sf "$terminalApp" ~/Desktop
+
+print
+red '#### Step 8 - Test it out! #########################################'
+
+print
+print 'Downloading the test Java program...'
+javaTestURL="${url_base}/${testFile}.java"
+javaTest="${myDir}/${testFile}.java"
+downloadQuiet "$javaTestURL" "$javaTest"
+
+coverURL="${url_base}/cover.jpg"
+cover="${myDir}/cover.jpg"
+downloadQuiet "$coverURL" "$cover"
+
+cd "$myDir"
+
+print
+print 'Installation complete! Compiling test program...'
+javac ${testFile}.java
+
+print 'Test program compiled. Running...'
+java ${testFile}
+
+rm -f "${testFile}.class"
+rm -f "${testFile}.java"
+rm -f "cover.jpg"
+
+print
+green 'If you saw the bullseye and textbook graphic, the installation'
+green 'was successful and you are ready to start programming in Java.'
+green 'Continue with the introductory tutorial on the website.'
+
+print
+red 'NOTE: If there were any error messages during this setup, check the'
+red 'troubleshooting section on the website or ask for help.'
+
+print
+print 'A log file of this installation is saved at'
+blue "${install}/log.txt"
+
+print
+green 'You should now close this window.'
