6becd1150b16e0ed7a59f941cc2c1d32b6df77f3
Hayk Martirosyan <hayk.mart@gmail.com>
2012-07-28 17:21:44 -0400

    Added email install counting, initial commit of findbugs and checkstyle batch files with unicode support

---
+++ introcs_windows/checkstyle.bat
@@ -0,0 +1,48 @@
+@echo off
+
+:: ***************************************************
+:: checkstyle.bat (introcs)
+:: Hayk Martirosyan
+:: ------------------
+:: Easy-to-understand wrapper for using checkstyle.
+:: ***************************************************
+
+:: Enables unicode support by changing the codepage
+for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
+chcp 1252 > NUL
+
+:: Sets the path to the install directory (done by the installer)
+set BASE=INSTALL_DIR
+
+:: Finds the checkstyle folder using a wildcard match (checkstyle-?.?)
+set FIND_FOLDER=dir /b "%BASE%"\checkstyle-?.?
+for /f "delims=" %%i in ('%FIND_FOLDER%') do set CHECKSTYLE=%%i
+
+:: Sets the paths to the checkstyle jar and xml files
+set JAR="%BASE%\%CHECKSTYLE%\%CHECKSTYLE%-all.jar"
+set XML="%BASE%\%CHECKSTYLE%\checkstyle.xml"
+
+:: If there are no arguments
+if [%1] equ [] (
+  echo Specify .java files as arguments.
+  echo Usage: 'checkstyle Test.java'
+  GOTO:END
+)
+
+:: If the first argument is a .java file that exists, runs checkstyle
+if [%~x1] equ [.java] (
+  if exist %1 (
+    echo Running checkstyle on %*:
+    java -jar %JAR% -c %XML% %*
+  ) else (
+    echo File not found! Make sure you are specifying the path correctly.
+    echo The filename is case sensitive.
+  )
+) else (
+  echo Checkstyle needs a .java file as an argument!
+  echo The filename is case sensitive.
+)
+
+:END
+:: Restores the original codepage
+chcp %cp% > NUL
---
+++ introcs_windows/findbugs.bat
@@ -0,0 +1,62 @@
+@echo off
+
+:: ***************************************************
+:: findbugs.bat (introcs)
+:: Hayk Martirosyan
+:: ------------------
+:: Easy-to-understand wrapper for using findbugs.
+:: ***************************************************
+
+:: Enables unicode support by changing the codepage
+for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
+chcp 1252 > NUL
+
+:: Sets the path to the install directory (done by the installer)
+set BASE=INSTALL_DIR
+
+:: Finds the findbugs folder using a wildcard match (findbugs-?.?.?)
+set FIND_FOLDER=dir /b /s "%BASE%"\findbugs-?.?.?
+for /f "delims=" %%i in ('%FIND_FOLDER%') do set FINDBUGS=%%i
+
+:: Sets the paths to the checkstyle jar and xml files
+set JAR="%FINDBUGS%\lib\findbugs.jar"
+set XML="%FINDBUGS%\findbugs.xml"
+set AUX="%BASE%\stdlib.jar;%BASE%\j3d\lib\ext\vecmath.jar;%BASE%\j3d\lib\ext\j3dcore.jar;%BASE%\j3d\lib\ext\j3dutils.jar;"
+
+:: If there are no arguments
+if [%1] equ [] (
+  echo Specify .class or .jar files as arguments.
+  echo Usage: 'findbugs Test.class'
+  GOTO:END
+)
+
+:: If the first argument is a .class file that exists, runs findbugs
+if [%~x1] equ [.class] (
+  if exist %1 (
+    echo Running findbugs on %*:
+    java -jar %JAR% -textui -longBugCodes -exclude %XML% -auxclasspath %AUX% %*
+  ) else (
+    echo File not found! Make sure you are specifying the path correctly.
+    echo The filename is case sensitive.
+  )
+  GOTO:END
+)
+
+:: If the first argument is a .jar file that exists, runs findbugs
+if [%~x1] equ [.jar] (
+  if exist %1 (
+    echo Running findbugs on %*:
+    java -jar %JAR% -textui -longBugCodes -exclude %XML% -auxclasspath %AUX% %*
+  ) else (
+    echo File not found! Make sure you are specifying the path correctly.
+    echo The filename is case sensitive.
+  )
+  GOTO:END
+)
+
+echo Findbugs needs .class or .jar files as arguments!
+echo The filename is case sensitive.
+
+:END
+:: Restores the original codepage
+chcp %cp% > NUL
