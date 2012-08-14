251aea220533aea2cd71ab57751e825b2d9c8ea6
Hayk Martirosyan <hayk.mart@gmail.com>
2012-08-13 23:34:21 -0400

    Rewrote findbugs/checkstile in PowerShell, changed 64-bit detection to size of pointer

--- algs4_windows/findbugs.bat
+++ algs4_windows/findbugs.bat
@@ -4,59 +4,22 @@
 :: findbugs.bat (algs4)
 :: Hayk Martirosyan
 :: ------------------
-:: Easy-to-understand wrapper for using findbugs.
+:: Launcher script for findbugs.ps1.
 :: ***************************************************
 
-:: Enables unicode support by changing the codepage
-for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
-chcp 1252 > NUL
+set SCRIPT=. '%~dp0\findbugs.ps1'
+set PS=%WINDIR%\System32\WindowsPowershell\v1.0\PowerShell.exe
 
-:: Sets the path to the install directory (done by the installer)
-set BASE=INSTALL_DIR
-
-:: Finds the findbugs folder using a wildcard match (findbugs-?.?.?)
-set FIND_FOLDER=dir /b /s "%BASE%"\findbugs-?.?.?
-for /f "delims=" %%i in ('%FIND_FOLDER%') do set FINDBUGS=%%i
-
-:: Sets the paths to the checkstyle jar and xml files
-set JAR="%FINDBUGS%\lib\findbugs.jar"
-set XML="%FINDBUGS%\findbugs.xml"
-set AUX="%BASE%\stdlib.jar;%BASE%\algs4.jar;"
-
-:: If there are no arguments
-if [%1] equ [] (
-  echo Specify .class or .jar files as arguments.
-  echo Usage: 'findbugs Test.class'
-  GOTO:END
+if exist "%PS%" (
+"%PS%" -ExecutionPolicy Unrestricted -NoProfile -Command "%SCRIPT%" %*
+) else (
+echo You do not have Microsoft PowerShell installed.
+echo Please run Windows Update.
+echo.
+echo If that does not help, manually install the
+echo Windows Management Framework Core
+echo.
+echo http://support.microsoft.com/kb/968929
+echo.
+pause
 )
-
-:: If the first argument is a .class file that exists, runs findbugs
-if [%~x1] equ [.class] (
-  if exist %1 (
-    echo Running findbugs on %*:
-    java -jar %JAR% -textui -longBugCodes -exclude %XML% -auxclasspath %AUX% %*
-  ) else (
-    echo File not found! Make sure you are specifying the path correctly.
-    echo The filename is case sensitive.
-  )
-  GOTO:END
-)
-
-:: If the first argument is a .jar file that exists, runs findbugs
-if [%~x1] equ [.jar] (
-  if exist %1 (
-    echo Running findbugs on %*:
-    java -jar %JAR% -textui -longBugCodes -exclude %XML% -auxclasspath %AUX% %*
-  ) else (
-    echo File not found! Make sure you are specifying the path correctly.
-    echo The filename is case sensitive.
-  )
-  GOTO:END
-)
-
-echo Findbugs needs .class or .jar files as arguments!
-echo The filename is case sensitive.
-
-:END
-:: Restores the original codepage
-chcp %cp% > NUL
--- introcs_windows/checkstyle.bat
+++ introcs_windows/checkstyle.bat
@@ -4,45 +4,22 @@
 :: checkstyle.bat (introcs)
 :: Hayk Martirosyan
 :: ------------------
-:: Easy-to-understand wrapper for using checkstyle.
+:: Launcher script for checkstyle.ps1.
 :: ***************************************************
 
-:: Enables unicode support by changing the codepage
-for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
-chcp 1252 > NUL
+set SCRIPT=. '%~dp0\checkstyle.ps1'
+set PS=%WINDIR%\System32\WindowsPowershell\v1.0\PowerShell.exe
 
-:: Sets the path to the install directory (done by the installer)
-set BASE=INSTALL_DIR
-
-:: Finds the checkstyle folder using a wildcard match (checkstyle-?.?)
-set FIND_FOLDER=dir /b "%BASE%"\checkstyle-?.?
-for /f "delims=" %%i in ('%FIND_FOLDER%') do set CHECKSTYLE=%%i
-
-:: Sets the paths to the checkstyle jar and xml files
-set JAR="%BASE%\%CHECKSTYLE%\%CHECKSTYLE%-all.jar"
-set XML="%BASE%\%CHECKSTYLE%\checkstyle.xml"
-
-:: If there are no arguments
-if [%1] equ [] (
-  echo Specify .java files as arguments.
-  echo Usage: 'checkstyle Test.java'
-  GOTO:END
-)
-
-:: If the first argument is a .java file that exists, runs checkstyle
-if [%~x1] equ [.java] (
-  if exist %1 (
-    echo Running checkstyle on %*:
-    java -jar %JAR% -c %XML% %*
-  ) else (
-    echo File not found! Make sure you are specifying the path correctly.
-    echo The filename is case sensitive.
-  )
+if exist "%PS%" (
+"%PS%" -ExecutionPolicy Unrestricted -NoProfile -Command "%SCRIPT%" %*
 ) else (
-  echo Checkstyle needs a .java file as an argument!
-  echo The filename is case sensitive.
+echo You do not have Microsoft PowerShell installed.
+echo Please run Windows Update.
+echo.
+echo If that does not help, manually install the
+echo Windows Management Framework Core
+echo.
+echo http://support.microsoft.com/kb/968929
+echo.
+pause
 )
-
-:END
-:: Restores the original codepage
-chcp %cp% > NUL
