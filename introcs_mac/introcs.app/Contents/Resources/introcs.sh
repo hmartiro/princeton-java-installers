0d53e039018afe319928b7072b60ca95340c8801
Hayk Martirosyan <hayk.mart@gmail.com>
2012-09-06 23:34:25 -0400

    explicitly call /usr/bin/which to avoid tcsh builtin which command

--- introcs_mac/introcs.app/Contents/Resources/introcs.sh
+++ introcs_mac/introcs.app/Contents/Resources/introcs.sh
@@ -175,7 +175,7 @@ python ${myDir}/counter.py 2> ${null}
 print
 red '#### Step 1 - Java #################################################'
 
-if which -s java ; then
+if /usr/bin/which -s java ; then
 	print
 	print 'Java is already installed.'
 else
